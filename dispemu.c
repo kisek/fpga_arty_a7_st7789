592a8f9148e0cab5158eb546217913049ba9463b
Kise K. <87082844+kisek@users.noreply.github.com>
2024-11-04 10:44:58 +0900

    Create dispemu.c

---
+++ dispemu.c
@@ -0,0 +1,79 @@
+/******************************************************************************/
+/* simple display_emulator ver. 0.0.6 for ST7789       ArchLab, Science Tokyo */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xutil.h>
+#include <cairo/cairo.h>
+#include <cairo/cairo-xlib.h>
+
+int size = 2;  /***** typical values are 1, 2, 3 *****/
+
+/******************************************************************************/
+void draw_rect(cairo_t *ca, int adr, int c) // cairo_t, adr, color
+{
+    int x = (adr) & 0xff;
+    int y = (adr) >> 8;
+    
+    int width = size;
+    int r1 = (c >> 11) & 0x3f;
+    int g1 = (c >>  5) & 0x3f;
+    int b1 = (c >>  0) & 0x1f;
+
+    cairo_set_source_rgb( ca, (float)r1/0x1f, (float)g1/0x1f, (float)b1/0x1f);
+    cairo_rectangle( ca, x*size, y*size, width, width );
+    cairo_fill( ca );
+}
+
+/******************************************************************************/
+/* format:@Daaaa_cc    (aaaa: address, cc: color)                             */
+/******************************************************************************/
+int main(int argc, char** argv)
+{
+    Display *display;
+    XEvent event;
+    Window win;
+    cairo_surface_t *cs;
+    cairo_t *c;
+
+    if(argc==2) size = atoi(argv[1]);
+    if(size<=0) size = 2;
+        
+    int width = 240*size, height = 240*size;
+    display = XOpenDisplay( NULL );
+    win = XCreateSimpleWindow( display,
+                               RootWindow( display, DefaultScreen(display) ),
+                               0, 0, width, height, 0,
+                               WhitePixel( display, DefaultScreen(display) ),
+                               BlackPixel( display, DefaultScreen(display) ) );
+    XMapWindow( display, win );
+    XStoreName( display, win, "Simple display emulator for RV-PC");
+    XSelectInput( display, win, ExposureMask );
+    while( 1 ){
+        XNextEvent( display, &event );
+        if( event.type == Expose ) break;
+    }
+
+    cs = cairo_xlib_surface_create( display, win, DefaultVisual(display,0),
+                                    width, height );
+    c = cairo_create( cs );
+
+    char str[4096];
+    int x_in, y_in, c_in;
+    while(1){
+        char *p = fgets(str, 4096, stdin);
+        if(str[0]=='@' && str[1]=='D'){
+            if(str[2]=='f' && str[3]=='i' && str[4]=='n') break;
+            sscanf(str, "@D%d_%d\n", &x_in, &c_in);
+            draw_rect( c, x_in, c_in);
+            XFlush( display );
+        }
+    }
+
+    cairo_destroy( c );
+    cairo_surface_destroy( cs );
+    XDestroyWindow( display, win );
+    XCloseDisplay( display );
+    return 0;
+}
+/******************************************************************************/
